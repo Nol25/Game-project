153f0a8a93cb0dc6e34ac55a072a5a6cd0f712e9
MCostell0 <25471783@studentmail.ul.ie>
2026-04-22 14:04:00 +0100

    Create Ragdoll.cs

---
+++ Code/Ragdoll.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ragdoll : MonoBehaviour
+{
+    Rigidbody[] rigidBodies;
+    Animator animator;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodies = GetComponentsInChildren<Rigidbody>();
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        DeactivateRagdoll();
+    }
+
+    public void DeactivateRagdoll()
+    {
+        foreach (var rigidBody in rigidBodies)
+        {
+            rigidBody.isKinematic = true;
+        }
+        animator.enabled = true;
+        agent.enabled = true;
+    }
+
+    public void ActivateRagdoll()
+    {
+        foreach (var rigidBody in rigidBodies)
+        {
+            rigidBody.isKinematic = false;
+        }
+        animator.enabled = false;
+        agent.enabled = false;
+    }
+
+    public void ApplyForce(Vector3 force)
+    {
+        var rigidBody = animator.GetBoneTransform(HumanBodyBones.Hips).GetComponent<Rigidbody>();
+        rigidBody.AddForce(force, ForceMode.VelocityChange);
+    }
+}
