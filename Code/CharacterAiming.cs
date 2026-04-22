02fee93c22b32073fbacdc681852ab9e4f98ab23
MCostell0 <25471783@studentmail.ul.ie>
2026-04-22 13:55:31 +0100

    Create CharacterAiming.cs

---
+++ Code/CharacterAiming.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public float turnSpeed = 15;
+    public float aimDuration = 0.3f;
+    public Rig aimLayer;
+
+    Camera mainCamera;
+    RaycastWeapon weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        weapon = GetComponentInChildren<RaycastWeapon>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Update()
+    {
+        if (aimLayer)
+        {
+            //  if (Input.GetButton("Fire2"))
+            //  {
+            //     aimLayer.weight += Time.deltaTime / aimDuration;
+            //  }
+            //  else
+            //  {
+            //      aimLayer.weight -= Time.deltaTime / aimDuration;
+            //  }
+            aimLayer.weight = 1;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            weapon.StartFiring();
+        }
+        if (weapon.isFiring)
+        {
+            weapon.UpdateFiring(Time.deltaTime);
+        }
+        weapon.UpdateBullets(Time.deltaTime);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            weapon.StopFiring();
+        }
+    }
+}
