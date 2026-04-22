1dad8a7d29c839eb2af0217c7b25c360d40ff72b
MCostell0 <25471783@studentmail.ul.ie>
2026-04-22 13:57:18 +0100

    Create Health.cs

---
+++ Code/Health.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    public float dieForce;
+    [HideInInspector]
+    public float currentHealth;
+    Ragdoll ragdoll;
+    SkinnedMeshRenderer skinnedMeshRenderer;
+    //UIHealthBar healthBar;
+
+    public float blinkIntensity;
+    public float blinkDuration;
+    float blinkTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ragdoll = GetComponent<Ragdoll>();
+        skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        //healthBar = GetComponentInChildren<UIHealthBar>();
+        currentHealth = maxHealth;
+
+        var rigidBodies = GetComponentsInChildren<Rigidbody>();
+        foreach (var rigidBody in rigidBodies)
+        {
+            HitBox hitBox = rigidBody.gameObject.AddComponent<HitBox>();
+            hitBox.health = this;
+        }
+    }
+
+    public void TakeDamage(float amount, Vector3 direction)
+    {
+        currentHealth -= amount;
+        //healthBar.SetHealthBarPercentage(currentHealth / maxHealth);
+        if (currentHealth <= 0.0f)
+        {
+            Die(direction);
+        }
+
+        blinkTimer = blinkDuration;
+    }
+
+    private void Die(Vector3 direction)
+    {
+        ragdoll.ActivateRagdoll();
+        direction.y = 1;
+        ragdoll.ApplyForce(direction * dieForce);
+       // healthBar.gameObject.SetActive(false);
+        skinnedMeshRenderer.updateWhenOffscreen = true;
+    }
+
+    private void Update()
+    {
+        blinkTimer -= Time.deltaTime;
+        float lerp = Mathf.Clamp01(blinkTimer / blinkDuration);
+        float intensity = (lerp * blinkIntensity) + 1.0f;
+        skinnedMeshRenderer.material.color = Color.white * intensity;
+    }
+}
